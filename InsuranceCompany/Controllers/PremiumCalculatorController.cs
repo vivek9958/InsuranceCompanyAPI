44f4fb18e29da07c9dc5938742dc3c307de0b7e6
VIVEKSINGH <vivek3221@outlook.com>
2021-05-11 18:38:03 +0530

    Minor fix

--- InsuranceCompany/Controllers/PremiumCalculatorController.cs
+++ InsuranceCompany/Controllers/PremiumCalculatorController.cs
@@ -1,4 +1,5 @@
 ﻿using InsuranceCompany.Models;
+using InsuranceCompany.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,10 +22,15 @@ namespace InsuranceCompany.Controllers
 
         [HttpPost]
         [Route("calculate")]
-        public long Post([FromBody]Client value)
-        {
-            int ratingFactor = 1;  // Create a dictionary with all the Occupation Rating and find the matching one to get the exact factor 
-            return (value.DeathSumInsured * ratingFactor * value.Age) / 1000 * 12;
+        public double Post([FromBody]Client value)
+        { 
+            if (CalculationService.GetOccupationRatings().ContainsKey(value.Occupation))
+            {
+                return ((value.DeathSumInsured * CalculationService.GetOccupationRatings()[value.Occupation] * value.Age) / 1000) * 12;
+            } else
+            {
+                return 0;  // Throw exception for input field
+            }
             //Death Premium = (Death Cover amount * Occupation Rating Factor * Age) /1000 * 12
 
         }
---
+++ InsuranceCompany/Services/CalculationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsuranceCompany.Services
+{
+    public static class CalculationService
+    {
+
+        public static IDictionary<string, double> GetOccupationRatings()
+        {
+            return new Dictionary<string, double>()
+           {
+               {"Professional", 1.0 },
+               {"White Collar", 1.25 },
+               {"Light Manual", 1.50 },
+               {"Heavy Manual", 1.75 }
+           };
+
+
+        }
+    }
+}
